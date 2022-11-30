3200da93b2e12074bb46cc54de398c967784c86f
Alexei Ruiz <alexeiruizrodri@gmail.com>
2022-11-30 08:42:08 -0300

    Estilos de Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [post, setPosts] = useState([]);
+  const getPost = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Ultimos Posts</h1>
+      {post.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        post.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className="btn">
+              Ler Mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
